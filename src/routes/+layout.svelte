c5cdc0f31f9b1d49c474c466865b9b28997238a9
Иванов Иван <danilka2712@yandex.ru>
2023-10-30 14:12:08 +0600

    first commit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,9 @@
   import "../app.postcss";
 </script>
 
-<header class="flex justify-between max-w-7xl mx-auto my-6 items-center">
+<header
+  class="flex justify-between xl:max-w-7xl max-w-6xl mx-auto my-6 items-center relative z-10"
+>
   <ul>
     <li class="flex gap-10">
       <a href="/">Главная</a>
@@ -10,7 +12,7 @@
     </li>
   </ul>
   <a class="w-fit" href="/">
-    <img src="logotype.svg" class="w-16 relative z-10" alt="logotype" />
+    <img src="logotype.svg" class="w-16" alt="logotype" />
   </a>
   <ul>
     <li class="flex gap-10">
@@ -23,3 +25,13 @@
 <main>
   <slot />
 </main>
+
+<footer
+  class="flex justify-center gap-4 xl:max-w-7xl max-w-6xl mx-auto my-12 items-center relative z-10"
+>
+  <a class="w-fit" href="/">
+    <img src="logotype.svg" class="w-16" alt="logotype" />
+  </a>
+
+  <span>разработка и продвижением сайтов</span>
+</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,43 +1,83 @@
 <script lang="ts">
+  import { afterUpdate, onMount } from "svelte";
+  import { gsap } from "gsap/dist/gsap";
+  import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+  import { slide } from "svelte/transition";
+  import { quintOut } from "svelte/easing";
+
+  onMount(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    gsap.to(".box", {
+      scrollTrigger: {
+        trigger: ".trigger",
+        start: "top 90%",
+        scrub: true,
+      },
+      y: -100,
+    });
+
+    gsap.to(".text", {
+      scrollTrigger: {
+        trigger: ".trigger",
+        start: "top 70%",
+        scrub: true,
+      },
+      opacity: 0,
+    });
+  });
+
+  let m = { x: 0, y: 0 };
+
+  function handleMousemove(event) {
+    m.x = event.clientX;
+    m.y = event.clientY;
+    setTimeout(() => {
+      imageHide = true;
+    }, 100);
+  }
+  let imageHide = false;
 </script>
 
-<div class="mx-auto max-w-7xl">
+<div class=" mx-auto opa xl:max-w-7xl max-w-6xl">
   <div
     class=" absolute flex -top-36 -rotate-12 items-center justify-center ml-auto mr-auto left-0 right-80 z-0"
   >
     <img
-      class="w-72 rounded-xl"
+      class="w-72 box rounded-xl"
       src="https://assets.website-files.com/64d1a3f882efd7fa3c726596/64dd44d77f953bd5852e5c53_Brand-007.webp"
       alt=""
     />
   </div>
   <div class=" absolute flex bottom-4 -right-44 -rotate-12 z-0">
     <img
-      class="w-72 rounded-xl"
+      class="w-72 box rounded-xl"
       src="https://assets.website-files.com/64d1a3f882efd7fa3c726596/64dd44d77f953bd5852e5c53_Brand-007.webp"
       alt=""
     />
   </div>
   <div class=" absolute flex bottom-56 -left-44 -rotate-12 z-0">
     <img
-      class="w-72 rounded-xl"
+      class="w-72 box rounded-xl"
       src="https://assets.website-files.com/64d1a3f882efd7fa3c726596/64dd44d77f953bd5852e5c53_Brand-007.webp"
       alt=""
     />
   </div>
   <div class="flex flex-col mt-32 relative z-10">
-    <h1 class="text-9xl -ml-2 font-extrabold">
+    <h1 class="lg:text-9xl text text-7xl -ml-2 font-extrabold">
       РАЗРАБОТКА <br /> И ПРОДВИЖЕНИЕ
     </h1>
-    <p class="text-2xl w-3/5 my-10 text-white/90">
+    <p class=" text-2xl text w-3/5 my-10 text-white/90">
       Мы занимаемся разработкой, проектированием, дизайном и продвижением
       уникальных цифровых продуктов, веб-сайтов, брендов и опыта.
     </p>
-    <button class="border p-2 px-6 w-fit rounded-full border-white/25"
+    <button class="border text p-2 px-6 w-fit rounded-full border-white/25"
       >Оставить заявку</button
     >
   </div>
-  <div class=" flex justify-between mt-28 border-t py-16 border-white/10">
+  <div
+    class="trigger flex justify-between mt-28 border-t py-16 border-white/10"
+  >
     <div class="flex justify-between w-full gap-20">
       <img
         class="w-72"
@@ -62,8 +102,31 @@
       />
     </div>
   </div>
-  <div class="flex flex-col justify-center items-center my-20 h-96">
-    <p class="text-center w-5/6 tracking-wide leading-tight text-5xl">
+  <div
+    role="button"
+    tabindex="0"
+    on:mouseleave={() => (imageHide = false)}
+    class="flex .scroll-trigger-ready__worm-wrap flex-col justify-center items-center relative my-20 h-96"
+  >
+    {#if imageHide}
+      <img
+        on:mouseleave={() => (imageHide = false)}
+        transition:slide|local={{
+          delay: 0,
+          duration: 200,
+          easing: quintOut,
+          axis: "x",
+        }}
+        class="w-72 absolute rounded-xl"
+        src="https://assets.website-files.com/64d293054a05b986902db3e3/64e53bb70695f0b7c417621f_packaging-box-tape-mockup-v4-isometric-1-2420x1615-p-800.jpeg"
+        style="left: {m.x - 180}px; top: {m.y - 280}px;"
+        alt=""
+      />
+    {/if}
+    <p
+      on:mousemove={handleMousemove}
+      class="text-center w-5/6 tracking-wide leading-tight text-5xl cursor-default"
+    >
       С момента начала своей карьеры фрилансера в 2014 году я сотрудничал с
       самыми разными клиентами, от процветающих стартапов до солидных
       корпораций.
@@ -72,7 +135,7 @@
       >Оставить заявку</button
     >
   </div>
-  <div class="my-32">
+  <div class="mt-32 mb-20">
     <p>Кейсы</p>
 
     <hr class="border-white/10 my-4" />
@@ -119,4 +182,87 @@
     </div>
     <hr class="border-white/10 my-4" />
   </div>
+
+  <div class="flex my-12">
+    <p class="">Соцсети <br /> и новости</p>
+    <div class="ml-20 flex gap-12">
+      <a class="text-5xl font-black" href="/">VK</a>
+
+      <a class="text-5xl font-black" href="/">INSTAGRAM</a>
+
+      <a class="text-5xl font-black" href="/">FACEBOOK</a>
+    </div>
+  </div>
+
+  <div class=" grid grid-cols-4 gap-10 my-24 mb-44">
+    <div class="flex flex-col gap-4">
+      <img
+        class=" rounded-xl h-80 object-cover"
+        src="https://assets.website-files.com/64d293054a05b986902db3e3/64e53bb70695f0b7c417621f_packaging-box-tape-mockup-v4-isometric-1-2420x1615-p-800.jpeg"
+        alt=""
+      />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, iste
+        porro ea cumque incidunt earum harum dolores enim, eveniet est.
+      </p>
+    </div>
+    <div class="flex flex-col gap-4">
+      <img
+        class=" rounded-xl h-80 object-cover"
+        src="https://assets.website-files.com/64d293054a05b986902db3e3/64e53bb70695f0b7c417621f_packaging-box-tape-mockup-v4-isometric-1-2420x1615-p-800.jpeg"
+        alt=""
+      />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, iste
+        porro ea cumque incidunt earum harum dolores enim, eveniet est magni
+        natus sint ratione recusandae eaque deserunt blanditiis tempore ipsam.
+      </p>
+    </div>
+    <div class="flex flex-col gap-4">
+      <img
+        class=" rounded-xl h-80 object-cover"
+        src="https://assets.website-files.com/64d293054a05b986902db3e3/64e53bb70695f0b7c417621f_packaging-box-tape-mockup-v4-isometric-1-2420x1615-p-800.jpeg"
+        alt=""
+      />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, iste
+        porro ea cumque incidunt earum harum dolores enim, eveniet est magni
+        natus sint ratione recusandae eaque deserunt blanditiis tempore ipsam.
+      </p>
+    </div>
+    <div class="flex flex-col gap-4">
+      <img
+        class=" rounded-xl h-80 object-cover"
+        src="https://assets.website-files.com/64d293054a05b986902db3e3/64e53bb70695f0b7c417621f_packaging-box-tape-mockup-v4-isometric-1-2420x1615-p-800.jpeg"
+        alt=""
+      />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, iste
+        porro ea cumque incidunt earum harum dolores enim, eveniet est magni
+        natus sint ratione recusandae eaque deserunt blanditiis tempore ipsam.
+      </p>
+    </div>
+  </div>
+  <div class="my-12 flex flex-col">
+    <p class="">Контакты</p>
+    <hr class="border-white/10 my-4" />
+
+    <div class="flex my-20 flex-col gap-10 justify-center items-center">
+      <span class=" tracking-wide font-bold leading-tight text-5xl text-center"
+        >Оставьте заявку <br /> или свяжитесь с нами <br /> по указанным контактам</span
+      >
+      <div class="flex flex-col justify-center gap-2 text-xl items-center">
+        <span class=" font-light text-white/75">telegram</span>
+        <a href="/" class="font-bold">@pitcher_dev</a>
+      </div>
+      <div class="flex flex-col text-xl justify-center gap-2 items-center">
+        <span class=" font-light text-white/75">e-mail</span>
+        <a href="/" class="font-bold">pitcher_dev@yandex.ru</a>
+      </div>
+      <button class=" border border-white/40 rounded-full p-2 px-4"
+        >Заполнить форму</button
+      >
+    </div>
+  </div>
 </div>
+<hr class="border-white/10 trigger my-4" />

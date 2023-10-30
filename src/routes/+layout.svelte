33d858b0be7635eb72a96000b8235efc364c5fb5
unknown <danilka2712@yandex.ru>
2023-10-30 09:34:49 +0600

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,25 @@
+<script>
+  import "../app.postcss";
+</script>
+
+<header class="flex justify-between max-w-7xl mx-auto my-6 items-center">
+  <ul>
+    <li class="flex gap-10">
+      <a href="/">Главная</a>
+      <a href="/">О нас</a>
+    </li>
+  </ul>
+  <a class="w-fit" href="/">
+    <img src="logotype.svg" class="w-16 relative z-10" alt="logotype" />
+  </a>
+  <ul>
+    <li class="flex gap-10">
+      <a href="/">Кейсы</a>
+      <a href="/">Контакты</a>
+    </li>
+  </ul>
+</header>
+
+<main>
+  <slot />
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,122 @@
+<script lang="ts">
+</script>
+
+<div class="mx-auto max-w-7xl">
+  <div
+    class=" absolute flex -top-36 -rotate-12 items-center justify-center ml-auto mr-auto left-0 right-80 z-0"
+  >
+    <img
+      class="w-72 rounded-xl"
+      src="https://assets.website-files.com/64d1a3f882efd7fa3c726596/64dd44d77f953bd5852e5c53_Brand-007.webp"
+      alt=""
+    />
+  </div>
+  <div class=" absolute flex bottom-4 -right-44 -rotate-12 z-0">
+    <img
+      class="w-72 rounded-xl"
+      src="https://assets.website-files.com/64d1a3f882efd7fa3c726596/64dd44d77f953bd5852e5c53_Brand-007.webp"
+      alt=""
+    />
+  </div>
+  <div class=" absolute flex bottom-56 -left-44 -rotate-12 z-0">
+    <img
+      class="w-72 rounded-xl"
+      src="https://assets.website-files.com/64d1a3f882efd7fa3c726596/64dd44d77f953bd5852e5c53_Brand-007.webp"
+      alt=""
+    />
+  </div>
+  <div class="flex flex-col mt-32 relative z-10">
+    <h1 class="text-9xl -ml-2 font-extrabold">
+      РАЗРАБОТКА <br /> И ПРОДВИЖЕНИЕ
+    </h1>
+    <p class="text-2xl w-3/5 my-10 text-white/90">
+      Мы занимаемся разработкой, проектированием, дизайном и продвижением
+      уникальных цифровых продуктов, веб-сайтов, брендов и опыта.
+    </p>
+    <button class="border p-2 px-6 w-fit rounded-full border-white/25"
+      >Оставить заявку</button
+    >
+  </div>
+  <div class=" flex justify-between mt-28 border-t py-16 border-white/10">
+    <div class="flex justify-between w-full gap-20">
+      <img
+        class="w-72"
+        src="https://toproi.ru/wp-content/uploads/2020/05/mcedelvise-1.svg
+        "
+        alt=""
+      />
+      <img
+        class="w-72"
+        src="https://toproi.ru/wp-content/uploads/2019/08/inrosko-logo.svg"
+        alt=""
+      />
+      <img
+        class="w-72"
+        src="https://toproi.ru/wp-content/uploads/2019/08/inrosko-logo.svg"
+        alt=""
+      />
+      <img
+        class="w-72"
+        src="https://toproi.ru/wp-content/uploads/2019/08/inrosko-logo.svg"
+        alt=""
+      />
+    </div>
+  </div>
+  <div class="flex flex-col justify-center items-center my-20 h-96">
+    <p class="text-center w-5/6 tracking-wide leading-tight text-5xl">
+      С момента начала своей карьеры фрилансера в 2014 году я сотрудничал с
+      самыми разными клиентами, от процветающих стартапов до солидных
+      корпораций.
+    </p>
+    <button class="border p-2 px-6 w-fit rounded-full border-white/25 mt-10"
+      >Оставить заявку</button
+    >
+  </div>
+  <div class="my-32">
+    <p>Кейсы</p>
+
+    <hr class="border-white/10 my-4" />
+
+    <div class="my-14 flex justify-between">
+      <p class="text-3xl w-1/4">
+        Ventura Airlines How we helped increase booking conversions
+      </p>
+      <div class="w-1/2">
+        <img
+          class=" rounded-xl"
+          src="https://assets.website-files.com/64d293054a05b986902db3e3/64e53bb70695f0b7c417621f_packaging-box-tape-mockup-v4-isometric-1-2420x1615-p-800.jpeg"
+          alt=""
+        />
+      </div>
+    </div>
+    <hr class="border-white/10 my-4" />
+
+    <div class="my-14 flex justify-between">
+      <p class="text-3xl w-1/4">
+        Ventura Airlines How we helped increase booking conversions
+      </p>
+      <div class="w-1/2">
+        <img
+          class=" rounded-xl"
+          src="https://assets.website-files.com/64d293054a05b986902db3e3/64e53bb70695f0b7c417621f_packaging-box-tape-mockup-v4-isometric-1-2420x1615-p-800.jpeg"
+          alt=""
+        />
+      </div>
+    </div>
+    <hr class="border-white/10 my-4" />
+
+    <div class="my-14 flex justify-between">
+      <p class="text-3xl w-1/4">
+        Ventura Airlines How we helped increase booking conversions
+      </p>
+      <div class="w-1/2">
+        <img
+          class=" rounded-xl"
+          src="https://assets.website-files.com/64d293054a05b986902db3e3/64e53bb70695f0b7c417621f_packaging-box-tape-mockup-v4-isometric-1-2420x1615-p-800.jpeg"
+          alt=""
+        />
+      </div>
+    </div>
+    <hr class="border-white/10 my-4" />
+  </div>
+</div>
